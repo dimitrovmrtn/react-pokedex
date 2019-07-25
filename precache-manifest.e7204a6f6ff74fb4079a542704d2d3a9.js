0ae41b4eaa361cebb1c11b78784a521b3357f4c7
Martin Dimitrov <dimitrovmrtn@gmail.com>
2019-07-25 17:39:02 +0300

    Updates

---
+++ precache-manifest.e7204a6f6ff74fb4079a542704d2d3a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022388769c8a22c786203c8772a18f7d",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "a6e5c49aceb053cab90b",
+    "url": "/react-pokedex/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "90591ba1babdb08937d0",
+    "url": "/react-pokedex/static/css/main.5f632f04.chunk.css"
+  },
+  {
+    "revision": "a6e5c49aceb053cab90b",
+    "url": "/react-pokedex/static/js/2.42455e02.chunk.js"
+  },
+  {
+    "revision": "90591ba1babdb08937d0",
+    "url": "/react-pokedex/static/js/main.282d31e9.chunk.js"
+  },
+  {
+    "revision": "49325ecd73a50f0ca547",
+    "url": "/react-pokedex/static/js/runtime~main.0848e284.js"
+  },
+  {
+    "revision": "2cb00cf704d424023d0f35a1a12117e8",
+    "url": "/react-pokedex/static/media/spinner.2cb00cf7.gif"
+  }
+]);
